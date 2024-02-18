15034ed540f1405a622386d22abdb3696a1525da
Volodymyr Nerovnia <nerv@i.ua>
2024-02-18 02:00:48 +0200

    Added menu to sidenav bar

--- src/app/core/shared/components/toolbar/toolbar.component.ts
+++ src/app/core/shared/components/toolbar/toolbar.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenav } from '@angular/material/sidenav';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatSidenav, MatSidenavModule, MatListModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
+  @ViewChild('snav') sidenav!: MatSidenav;
 
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 }
