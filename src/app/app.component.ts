2b298139c35671606004b4d1bab3e6d0818f12f3
Volodymyr Nerovnia <nerv@i.ua>
2024-02-22 01:39:27 +0200

    Added components for Companies reference book: - table-list - input-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './core/shared/components/toolbar/toolbar.component';
+import { InputFormComponent } from './core/shared/components/ref-books/companies/input-form/input-form.component';
+import { TableListComponent } from './core/shared/components/ref-books/companies/table-list/table-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, InputFormComponent, TableListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/core/shared/components/ref-books/companies/table-list/table-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-list.component.html',
+  styleUrl: './table-list.component.scss'
+})
+export class TableListComponent {
+
+}
