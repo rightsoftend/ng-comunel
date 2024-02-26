a595d55c1f2d2355a2bd1674c91048fddf2d1ca5
Volodymyr Nerovnia <nerv@i.ua>
2024-02-26 02:31:58 +0200

    Added count types test data

---
+++ src/test/data/count-types.ts
@@ -0,0 +1,15 @@
+interface CountType {
+  id: number;
+  name: string;
+}
+
+export const countTypes: CountType[] = [
+  {
+    id: 1,
+    name: 'лічильник'
+  },
+  {
+    id: 2,
+    name: 'період'
+  },
+];
